4366fb7a0249d297d757614db0d0c7d2798adcbb
songmy <songmy.it@qq.com>
2018-12-29 17:58:23 +0800

    export metric

--- oap-server/server-prometheus/src/main/java/org/apache/skywalking/oap/server/prometheus/metric/GaugeMetricService.java
+++ oap-server/server-prometheus/src/main/java/org/apache/skywalking/oap/server/prometheus/metric/GaugeMetricService.java
@@ -9,10 +9,10 @@ import org.apache.skywalking.oap.server.core.source.EndpointRelation;
 public enum GaugeMetricService {
     INSTANCE;
 
-    private static final Gauge GAUGE = Gauge.build().name("neunn_paas_sw_request_latency").help("调用延迟统计")
+    private static final Gauge GAUGE = Gauge.build().name("neunn_paas_sw_request_latency_gauge").help("调用延迟统计")
             .labelNames(CounterMetricService.segmentLabelNames).register();
 
     public void metric(EndpointRelation endpointRelation) {
-        CounterMetricService.INSTANCE.labels(GAUGE, endpointRelation).set(endpointRelation.getRpcLatency());
+        GAUGE.labels(CounterMetricService.INSTANCE.labels(endpointRelation)).set(endpointRelation.getRpcLatency());
     }
 }
--- oap-server/server-prometheus/src/main/java/org/apache/skywalking/oap/server/prometheus/metric/HistogramMetricService.java
+++ oap-server/server-prometheus/src/main/java/org/apache/skywalking/oap/server/prometheus/metric/HistogramMetricService.java
@@ -1,13 +1,18 @@
 package org.apache.skywalking.oap.server.prometheus.metric;
 
 import io.prometheus.client.Histogram;
+import org.apache.skywalking.oap.server.core.source.EndpointRelation;
 
 /**
  * @author songmy
  */
 public enum HistogramMetricService {
     INSTANCE;
-    public static final Histogram HISTOGRAM = Histogram.build().name("").help("")
+    public static final Histogram HISTOGRAM = Histogram.build().name("neunn_paas_sw_request_latency_histogram").help("调用响应时间统计")
             .labelNames(CounterMetricService.segmentLabelNames)
             .buckets(0, 100, 200, 500, 1000, 1500, 2000, 2500, 3000, 3500).register();
+
+    public void metric(EndpointRelation endpointRelation){
+        HISTOGRAM.labels(CounterMetricService.INSTANCE.labels(endpointRelation)).observe(endpointRelation.getRpcLatency());
+    }
 }
