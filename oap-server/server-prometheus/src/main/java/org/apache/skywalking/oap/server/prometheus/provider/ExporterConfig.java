119b2444c7eb73a7d7422280fb2ea1481cd71e1c
songmy <songmy.it@qq.com>
2019-01-15 16:29:07 +0800

    delete

--- oap-server/server-prometheus/src/main/java/org/apache/skywalking/oap/server/prometheus/provider/ExporterConfig.java
+++
@@ -1,9 +0,0 @@
-package org.apache.skywalking.oap.server.prometheus.provider;
-
-import org.apache.skywalking.oap.server.library.module.ModuleConfig;
-
-/**
- * @author songmy
- */
-public class ExporterConfig extends ModuleConfig {
-}
